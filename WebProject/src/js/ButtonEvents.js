cff9e7afff884c9fa6ccecbf154d6361ce50a86a
apetron <apetron@oakland.edu>
2022-10-23 21:35:18 -0400

    Made progress, properties menu now shows properties. Need to figure out how to apply them.

--- WebProject/src/js/ButtonEvents.js
+++ WebProject/src/js/ButtonEvents.js
@@ -1,4 +1,44 @@
-var selectedObject = null;
+var selectedIndex = 0;
+
+//From https://stackoverflow.com/a/5624139
+function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+//Input must be a 3 digit hex string, including the # at the start
+function expandShortHex(shortHex) {
+    let shortHexDigits = shortHex.replace("#", "");
+    let hexChars = shortHex.replace("#", "").split("");
+    if (hexChars.length == 3)
+        return (
+            "#" +
+            hexChars[0] +
+            hexChars[0] +
+            hexChars[1] +
+            hexChars[1] +
+            hexChars[2] +
+            hexChars[2]
+        );
+    else return shortHex;
+}
+
+//From https://stackoverflow.com/a/5624139
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+        ? {
+              r: parseInt(result[1], 16),
+              g: parseInt(result[2], 16),
+              b: parseInt(result[3], 16),
+          }
+        : null;
+}
 
 function drawLine(canvasId) {
     let c = document.getElementById(canvasId);
@@ -11,7 +51,6 @@ function drawLine(canvasId) {
 function drawRectangleClicked() {
     $("canvas").drawRect({
         layer: true,
-        draggable: true,
         name: "myBox",
         fillStyle: "#585",
         x: 100,
@@ -21,27 +60,22 @@ function drawRectangleClicked() {
         click: function (layer) {
             displayProperties(layer, false);
         },
-        dragstop: function (layer) {
-            layer.draggable = true;
-        },
     });
 }
 
-function drawCircleClicked(canvasId) {
+function drawEllipseClicked(canvasId) {
     $("canvas").drawEllipse({
         layer: true,
-        draggable: true,
         name: "circle",
-        fillStyle: "#585",
+        fillStyle: "#654",
+        strokeStyle: "#000",
+        strokeWidth: "3",
         x: 100,
         y: 100,
         width: 100,
         height: 100,
         click: function (layer) {
             displayProperties(layer, false);
-        },
-        dragstop: function (layer) {
-            layer.draggable = true;
         },
     });
 }
@@ -58,15 +92,39 @@ function clearCanvas(canvasId, text) {
         let c = document.getElementById(canvasId);
         let ctx = c.getContext("2d");
         ctx.clearRect(0, 0, c.clientWidth, c.clientHeight);
+        $("canvas").removeLayers();
     }
 }
 
 function displayProperties(layer, isText) {
+    selectedIndex = layer.index;
+
     $("#properties").css("display", "inline-block");
+
     if (isText) {
         $("#text").css("display", "block");
+        $("#text").val(layer.text);
     } else {
         $("#text").css("display", "none");
     }
+
+    //console.log($("#fillColorID"));
+    $("#widthID").val(layer.width);
+    $("#heightID").val(layer.height);
+    $("#borderWidthID").val(layer.strokeWidth);
+    $("#borderColorID").val(expandShortHex(layer.strokeStyle));
+    $("#fillColorID").val(expandShortHex(layer.fillStyle));
+    //Need to convert colors to RGB, then back to hex for setting the style
     //TODO, set values in #properties based on layer.
+}
+
+function applyPropertiesClicked() {
+    $("canvas").setLayer(selectedIndex, {
+        width: $("#widthID").value,
+        height: $("#heightID").value,
+        strokeWidth: $("#borderWidthID").value,
+        strokeStyle: $("#borderColorID").value,
+        fillStyle: $("#fillColorID").value,
+        text: $("#text").value,
+    });
 }
